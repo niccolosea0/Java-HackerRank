34f7bbd3dafd5299897881e0f9452ef34d052451
niccolosea0 <niccolosea@gmail.com>
2026-01-26 20:03:39 +0400

    Add Solution.java for ex6

---
+++ 06_loops_II/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Solution{
+    public static void main(String []argh){
+
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+
+        for(int i = 0; i < t; i++){
+            
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int s = a;
+            
+            for (int j = 0; j < n; j++) {
+                s += (Math.pow(2, j) * b);
+                System.out.print(s + " ");
+            }
+            
+            // Print new line
+            System.out.println();
+        }
+        in.close();
+    }
+}
