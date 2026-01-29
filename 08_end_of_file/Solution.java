8b15f9e6778e76e63dfa2a40fd1334e03ceea74c
niccolosea0 <niccolosea@gmail.com>
2026-01-29 11:48:55 +0400

    Add submitted version in comments

--- 08_end_of_file/Solution.java
+++ 08_end_of_file/Solution.java
@@ -27,3 +27,23 @@ public class Solution {
     }
 }
 
+/*
+// Submitted version
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        
+        while (scanner.hasNextLine()) {
+            counter++;
+            System.out.println(counter + " " + scanner.nextLine());
+        }
+        
+        scanner.close();
+    }
+}
+*/
